31ef444c83c9f793b5df220f0b00640baf9dd70e
harish29k <harishkodumuri@gmail.com>
2021-10-11 11:30:40 +0530

    starting game from 0

---
+++ Startposition0UC1.java
@@ -0,0 +1,17 @@
+package snakeandladers;
+
+
+	/*
+	*Snake and ladder game played with single player at start position 0
+	*
+	*/
+	public class Startposition0UC1 {
+		public static void main(String[] args) {
+
+			// starting position of player
+			int startPosition = 0;
+			System.out.println(" start position is: " + startPosition);
+		}
+
+	}
+
